821d799977254ec6fdf550acb750fa37ad5f4fea
RenanMsV <r.delaneza@gmail.com>
2025-04-06 23:53:58 -0300

    Update StatefulImageButtonClass
    - Add parameter type annotations
    - Add docstrings for each function
    - Switch to CamelCase keeping backwards compatibility

--- src/Components/StatefulImageButton.lua
+++ src/Components/StatefulImageButton.lua
@@ -7,89 +7,105 @@
 -- Uses shading to indicate hover and click states.
 --
 ----------------------------------------
-GuiUtilities = require(script.Parent.GuiUtilities)
+GuiUtilities = require("../GuiUtilities")
 
 StatefulImageButtonClass = {}
 StatefulImageButtonClass.__index = StatefulImageButtonClass
 
-function StatefulImageButtonClass.new(buttonName, imageAsset, buttonSize)
-	local self = {}
-	setmetatable(self, StatefulImageButtonClass)
+--- StatefulImageButtonClass constructor.
+--- @param buttonName string -- The name of the button instance.
+--- @param imageAsset string -- The asset ID or path for the button's image.
+--- @param buttonSize UDim2 -- The size of the button.
+--- @return StatefulImageButtonClass -- A new instance of the StatefulImageButtonClass.
+function StatefulImageButtonClass.new(buttonName: string, imageAsset: string, buttonSize: UDim2)
+  local self = {}
+  setmetatable(self, StatefulImageButtonClass)
 
-	local button = Instance.new("ImageButton")
-	button.Parent = parent
-	button.Image = imageAsset
-	button.BackgroundTransparency = 1
-	button.BorderSizePixel = 0
-	button.Size = buttonSize
-	button.Name = buttonName
+  local button = Instance.new("ImageButton")
+  --button.Parent = parent
+  button.Image = imageAsset
+  button.BackgroundTransparency = 1
+  button.BorderSizePixel = 0
+  button.Size = buttonSize
+  button.Name = buttonName
 
-	self._button = button
+  self._button = button
 
-	self._hovered = false
-	self._clicked = false
-	self._selected = false
+  self._hovered = false
+  self._clicked = false
+  self._selected = false
 
-	button.InputBegan:connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseMovement) then               
-			self._hovered = true
-			self:_updateButtonVisual()
-		end
-	end)
+  button.InputBegan:Connect(function(input)
+    if (input.UserInputType == Enum.UserInputType.MouseMovement) then               
+      self._hovered = true
+      self:_UpdateButtonVisual()
+    end
+  end)
 
 
-	button.InputEnded:connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseMovement) then               
-			self._hovered = false
-			self._clicked = false
-			self:_updateButtonVisual()
-		end
-	end)    
+  button.InputEnded:Connect(function(input)
+    if (input.UserInputType == Enum.UserInputType.MouseMovement) then               
+      self._hovered = false
+      self._clicked = false
+      self:_UpdateButtonVisual()
+    end
+  end)    
 
-	button.MouseButton1Down:connect(function()
-		self._clicked = true
-		self:_updateButtonVisual()
-	end)
+  button.MouseButton1Down:Connect(function()
+    self._clicked = true
+    self:_UpdateButtonVisual()
+  end)
 
-	button.MouseButton1Up:connect(function()
-		self._clicked = false
-		self:_updateButtonVisual()
-	end)
-	
-	self:_updateButtonVisual()
+  button.MouseButton1Up:Connect(function()
+    self._clicked = false
+    self:_UpdateButtonVisual()
+  end)
+  
+  self:_UpdateButtonVisual()
 
-	return self
+  return self
 end
 
-function StatefulImageButtonClass:_updateButtonVisual()
-	if (self._selected) then 
-		self._button.ImageTransparency = 0
-		self._button.ImageColor3 = Color3.new(1,1,1) 
-	else 
-		self._button.ImageTransparency = 0.5
-		self._button.ImageColor3 = Color3.new(.5,.5,.5)
-	end
+-- Backwards compatibility (should be removed in the future)
+StatefulImageButtonClass.setSelected = StatefulImageButtonClass.SetSelected
+StatefulImageButtonClass.getSelected = StatefulImageButtonClass.GetSelected
+StatefulImageButtonClass.getButton = StatefulImageButtonClass.GetButton
 
-	if (self._clicked) then 
-		self._button.BackgroundTransparency = 0.8
-	elseif (self._hovered) then 
-		self._button.BackgroundTransparency = 0.9
-	else
-		self._button.BackgroundTransparency = 1
-	end
+function StatefulImageButtonClass:_UpdateButtonVisual()
+  if (self._selected) then 
+    self._button.ImageTransparency = 0
+    self._button.ImageColor3 = Color3.new(1,1,1) 
+  else 
+    self._button.ImageTransparency = 0.5
+    self._button.ImageColor3 = Color3.new(.5,.5,.5)
+  end
+
+  if (self._clicked) then 
+    self._button.BackgroundTransparency = 0.8
+  elseif (self._hovered) then 
+    self._button.BackgroundTransparency = 0.9
+  else
+    self._button.BackgroundTransparency = 1
+  end
 end
 
-function StatefulImageButtonClass:setSelected(selected)
-	self._selected = selected
-	self:_updateButtonVisual()
+--- Sets the selection state of the button.
+--- @param selected boolean -- Whether the button should appear selected.
+function StatefulImageButtonClass:SetSelected(selected)
+  self._selected = selected
+  self:_updateButtonVisual()
 end
 
-function StatefulImageButtonClass:getSelected()
-	return self._selected
+--- Gets the current selection state of the button.
+--- @return boolean -- True if the button is selected, false otherwise.
+function StatefulImageButtonClass:GetSelected()
+  return self._selected
 end
 
-function StatefulImageButtonClass:getButton()
-	return self._button
+--- Returns the underlying ImageButton instance.
+--- @return ImageButton -- The ImageButton instance.
+function StatefulImageButtonClass:GetButton()
+  return self._button
 end
 
 return StatefulImageButtonClass
