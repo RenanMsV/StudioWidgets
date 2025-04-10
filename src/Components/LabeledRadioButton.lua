aa12ea21414dbed8716828e0021a3c300e779f7b
RenanMsV <r.delaneza@gmail.com>
2025-04-09 23:26:30 -0300

    Updated LabeledRadioButtonClass
    - New visuals
    - Add parameters type annotations
    - Add docstrings for  each function

--- src/Components/LabeledRadioButton.lua
+++ src/Components/LabeledRadioButton.lua
@@ -5,56 +5,51 @@
 -- Creates a frame containing a label and a radio button.
 --
 ----------------------------------------
-GuiUtilities = require(script.Parent.GuiUtilities)
-LabeledCheckbox = require(script.Parent.LabeledCheckbox)
+GuiUtilities = require("../GuiUtilities")
+LabeledCheckbox = require("./LabeledCheckbox")
 
-local kButtonImage = "rbxasset://textures/TerrainTools/radio_button_frame.png"
-local kBulletImage = "rbxasset://textures/TerrainTools/radio_button_bullet.png"
-
-local kButtonImageDark = "rbxasset://textures/TerrainTools/radio_button_frame_dark.png"
-local kBulletImageDark = "rbxasset://textures/TerrainTools/radio_button_bullet_dark.png"
-
-local kFrameSize = 12
-local kBulletSize = 14
+local kDefaultImage = "rbxasset://textures/RoactStudioWidgets/button_radiobutton_default.png"
+local kEnabledImageLight = "rbxasset://textures/RoactStudioWidgets/button_radiobutton_chosen.png"
+local kEnabledImageDark = kEnabledImageLight
+local kRadioFrameImage = kDefaultImage
 
 LabeledRadioButtonClass = {}
 LabeledRadioButtonClass.__index = LabeledRadioButtonClass
 setmetatable(LabeledRadioButtonClass, LabeledCheckbox)
 
-function LabeledRadioButtonClass.new(nameSuffix, labelText)
-	local newButton = LabeledCheckbox.new(nameSuffix, labelText, false)
-	setmetatable(newButton, LabeledRadioButtonClass)
+--- LabeledRadioButtonClass constructor.
+--- @param nameSuffix string -- Suffix to append to the name of the UI elements.
+--- @param labelText string -- The label displayed next to the radio button.
+--- @return LabeledRadioButtonClass -- A new instance of the labeled radio button class.
+function LabeledRadioButtonClass.new(nameSuffix: string, labelText: string)
+  local newButton = LabeledCheckbox.new(nameSuffix, labelText, false)
+  setmetatable(newButton, LabeledRadioButtonClass)
 
-	newButton:UseSmallSize()
-	newButton._checkImage.Position = UDim2.new(0.5, 0, 0.5, 0)
-	newButton._checkImage.Image = kBulletImage
-	newButton._checkImage.Size = UDim2.new(0, kBulletSize, 0, kBulletSize)
+  newButton:GetFrame().Name = "RBF" .. nameSuffix
+  newButton:UseSmallSize()
+  newButton._checkImage.Position = UDim2.new(0.5, 0, 0.5, 0)
+  newButton._checkImage.Image = ""
+  newButton._button.Image = kRadioFrameImage
 
-	newButton._button.Image = kButtonImage
-	newButton._button.Size = UDim2.new(0, kFrameSize, 0, kFrameSize)
-	newButton._button.BackgroundTransparency = 1
-
-	local function updateImages()
-		if (GuiUtilities:ShouldUseIconsForDarkerBackgrounds()) then 
-			newButton._checkImage.Image = kBulletImageDark
-			newButton._button.Image = kButtonImageDark
-		else
-			newButton._checkImage.Image = kBulletImage
-			newButton._button.Image = kButtonImage
-		end
-	end
-	settings().Studio.ThemeChanged:connect(updateImages)
-	updateImages()
-
-	return newButton
+  return newButton
 end
 
 function LabeledRadioButtonClass:_MaybeToggleState()
-	-- A checkbox can never be toggled off. 
-	-- Only turns off because another one turns on.
-	if (not self._disabled and not self._value) then 
-		self:SetValue(not self._value)
-	end
+  -- A radio can never be toggled off. 
+  -- Only turns off because another one turns on.
+  if (not self._disabled and not self._value) then 
+    self:SetValue(not self._value)
+  end
+end
+
+function LabeledRadioButtonClass:_UpdateAppearance()
+  if self:GetValue() then
+    self._checkImage.Image = if GuiUtilities.GetThemeName() == "Light" then kEnabledImageLight else kEnabledImageDark
+    self._checkImage.ImageColor3 = if GuiUtilities.GetThemeName() == "Light" then Color3.fromRGB(219, 219, 219) else Color3.new(1, 1, 1)
+  else
+    self._checkImage.Image = ""
+    self._button.ImageColor3 = Color3.fromRGB(180, 180, 180)
+  end
 end
 
 return LabeledRadioButtonClass
